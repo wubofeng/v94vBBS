8358f62ad20ed97f9accebe61ea124cc4b5cde88
EwenSal <wubofeng@vip.qq.com>
2018-09-18 15:45:12 +0000

    Notify author when topic replied

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,6 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+
+// 通知路由
+Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
