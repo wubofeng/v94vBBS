bb316f743dc4159f10f341b324397cb509592274
EwenSal <wubofeng@vip.qq.com>
2018-09-18 14:45:51 +0000

    User can create reply

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Topic;
+use App\Models\Category;
 
 class TestController extends Controller
 {
-    //
-    public function index()
+    public function index(Category $category, Request $request, Topic $topic)
     {
-    	var_dump(config('app.url'));
+    	var_dump($topic->user->avatar);
     }
 }
