7ee17ef0d0d7d13181b0d9d1ab25b3c61deb1822
wubofeng-company <wubofeng@vip.qq.com>
2018-10-16 11:09:07 +0800

    去掉首页边栏

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -12,7 +12,7 @@
 
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
-                v94vBBS
+                律助天地
             </a>
         </div>
 
