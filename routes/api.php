b11aa48f3b0c733dfd0848a2b8a98c850f05d0dc
wubofeng-company <wubofeng@vip.qq.com>
2018-11-02 15:29:57 +0800

    api/user

--- routes/api.php
+++ routes/api.php
@@ -16,13 +16,15 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
+    'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
 ], function($api) {
     $api->group([
         'middleware' => 'api.throttle',
         'limit' => config('api.rate_limits.sign.limit'),
         'expires' => config('api.rate_limits.sign.expires'),
     ], function ($api){
+        // 游客可访问的接口
         // 短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store')
             ->name('api.verificationCodes.store');
@@ -50,6 +52,13 @@ $api->version('v1', [
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
+
+        // 需要token验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api){
+            // 当前登录用户
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
     });
 
 });
