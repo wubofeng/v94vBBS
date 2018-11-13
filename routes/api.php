2b8e4aeba1432f78b80ef94cbccbdf3cd7579571
wubofeng-company <wubofeng@vip.qq.com>
2018-11-13 15:51:52 +0800

    weapp update user

--- routes/api.php
+++ routes/api.php
@@ -96,6 +96,9 @@ $api->version('v1', [
             // 编辑登录用户信息
             $api->patch('user', 'UsersController@update')
                 ->name('api.user.update');
+            // 小程序编辑用户信息
+            $api->put('user', 'UsersController@update')
+                ->name('api.user.update');
 
             // 图片资源
             $api->post('images', 'ImagesController@store')
