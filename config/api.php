a2e5e37c29222be87311122b47a7247aed244ed8
wubofeng-company <wubofeng@vip.qq.com>
2018-11-14 19:16:00 +0800

    修改请求次数

--- config/api.php
+++ config/api.php
@@ -236,7 +236,7 @@ return [
         // 访问频率限制，次数/分钟
         'access' => [
             'expires' => env('RATE_LIMITS_EXPIRES', 1),
-            'limit'  => env('RATE_LIMITS', 60),
+            'limit'  => env('RATE_LIMITS', 600),
         ],
         // 登录相关，次数/分钟
         'sign' => [
